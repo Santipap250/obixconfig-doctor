e86d978ce2d8c532a938f74d89793c81826db1f6
Santipap250 <santipap350z@gmail.com>
2026-01-04 22:29:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -109,5 +109,11 @@ def index():
 # ===============================
 # RUN
 # ===============================
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 10000)),
+        debug=False
+    )
